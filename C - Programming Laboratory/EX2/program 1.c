e0efa86673c3a0d53fa641f59ba81a9ede15b577
Abbilaash A T <78356376+Abbilaash@users.noreply.github.com>
2024-02-29 13:58:55 +0530

    Update program 1.c

--- C - Programming Laboratory/EX2/program 1.c
+++ C - Programming Laboratory/EX2/program 1.c
@@ -1,1 +1,11 @@
+#include <stdio.h>
+#include <math.h>
 
+void main()
+{
+    float var;
+    float val;
+    printf("Enter temperature in C:");
+    scanf("%f",&var);
+    printf("The temperature in F: %f",(var*9/5)+32);
+}
