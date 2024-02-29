6a53f63f574a652fabefe5d93657538f4d02733e
Abbilaash A T <78356376+Abbilaash@users.noreply.github.com>
2024-02-29 14:18:05 +0530

    Create program 3.c

---
+++ C - Programming Laboratory/EX2/program 3.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+void main()
+{
+    float height,width;
+    printf("Enter the height:");
+    scanf("%f",&height);
+    printf("Enter the width:");
+    scanf("%f",&width);
+    printf("The perimeter if the rectangle is: %f",(2.0*height)+(2.0*width));
+}
