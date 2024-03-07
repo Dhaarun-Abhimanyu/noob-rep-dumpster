d8bdb85204894b441da81d51bb822faf3eb2b3f3
Dhaarun <dhaarunabhimanyu@gmail.com>
2024-03-07 14:05:44 +0530

    test-commit-dhaaurn

--- C - Programming Laboratory/EX2/program 9.c
+++ C - Programming Laboratory/EX2/program 9.c
@@ -8,6 +8,5 @@ void main() {
     printf("The sum of the two numbers: %d",num1+num2);
     printf("\nThe difference of the two numbers: %d",num1-num2);
     printf("\nThe product of the two numbers: %d",num1*num2);
-    div = (float)num1/num2;
-    printf("\nThe quotient of the two numbers: %f",div);
+    printf("\nThe quotient of the two numbers: %f",((float)num1/num2));
 }
