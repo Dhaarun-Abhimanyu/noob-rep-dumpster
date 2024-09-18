130913fd3de6cc3820197c392a70d491f9a9c204
Abbilaash A T <78356376+Abbilaash@users.noreply.github.com>
2024-09-18 12:10:24 +0530

    Update insertion.c

--- abbi-playground/insertion.c
+++ abbi-playground/insertion.c
@@ -1,9 +1,8 @@
-
 #include <stdio.h>
 
-int insertion()
+int main()
 {
-    int n,i,key,j,k;
+    int n,i,key,j,k,icounter=0;
     printf("Enter number of values: ");
     scanf("%d",&n);
     int arr[n];
@@ -14,13 +13,21 @@ int insertion()
         key = arr[i];
         j = i - 1;
         while (j >= 0 && arr[j] > key) {
+            icounter++;
             arr[j + 1] = arr[j];
             j = j - 1;
         }
         arr[j + 1] = key;
     }
     for (i=0;i<n;i++){
-        printf("%d",arr[i]);
+        printf("%d,",arr[i]);
     }
+    if (icounter == 0)
+        icounter = n-1;
+
+    printf("\nTotal number of iterations to sort: %d",icounter);
+    printf("\nBest case: %d",n-1);
+    printf("\nWorst case: %d",((n-1)*n)/2);
+    printf("\nAverage case: %d",((n-1)*n)/2);
     return 0;
 }
