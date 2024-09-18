290b73dfa6330b872f7d331f29c35d4a6c9035cf
Abbilaash A T <78356376+Abbilaash@users.noreply.github.com>
2024-09-18 11:48:16 +0530

    Update selection.c

--- abbi-playground/selection.c
+++ abbi-playground/selection.c
@@ -1,10 +1,15 @@
-
-
 #include <stdio.h>
 
-int selection()
+void swap(int *xp, int *yp)
 {
-    int n,i,key,j,k;
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
+int main()
+{
+    int n,i,key,j,k,min_idx,icounter=0,ccounter=0;
     printf("Enter number of values: ");
     scanf("%d",&n);
     int arr[n];
@@ -12,10 +17,25 @@ int selection()
         scanf("%d",&arr[k]);
     }
 
-
-
-    for (i=0;i<n;i++){
-        printf("%d",arr[i]);
+    for (i = 0; i < n-1; i++){
+        min_idx = i;
+        for (j = i+1; j < n; j++){
+            icounter++;
+            if (arr[j] < arr[min_idx]){
+                min_idx = j;
+            }
+        }
+        if(min_idx != i){
+            swap(&arr[min_idx], &arr[i]);
+        }
     }
+    for (i=0;i<n;i++){
+        printf("%d,",arr[i]);
+    }
+
+    printf("\nTotal number of iterations to sort: %d",icounter);
+    printf("\nBest case: %d",n-1);
+    printf("\nWorst case: %d",((n-1)*n)/2);
+    printf("\nAverage case: %d",((n-1)*n)/2);
     return 0;
 }
